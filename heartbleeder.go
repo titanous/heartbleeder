1e174cc8c1d7e0b141b37268c40780d8c910cb77
Jonathan Rudenberg <jonathan@titanous.com>
2014-04-08 10:40:52 -0400

    Fix ErrNoHeartbeat check

--- heartbleeder.go
+++ heartbleeder.go
@@ -25,8 +25,13 @@ func main() {
 		os.Exit(2)
 	}
 
-	if err := c.WriteHeartbeat(32, nil); err != nil {
-		fmt.Printf("UNKNOWN - Heartbeat enabled, but there was an error writing the payload:", err)
+	err = c.WriteHeartbeat(32, nil)
+	if err == tls.ErrNoHeartbeat {
+		fmt.Printf("SECURE - %s does not have the heartbeat extension enabled\n", host)
+		os.Exit(0)
+	}
+	if err != nil {
+		fmt.Println("UNKNOWN - Heartbeat enabled, but there was an error writing the payload:", err)
 		os.Exit(2)
 	}
 
@@ -38,16 +43,12 @@ func main() {
 
 	select {
 	case err := <-readErr:
-		switch err {
-		case nil:
+		if err == nil {
 			fmt.Printf("VULNERABLE - %s has the heartbeat extension enabled and is vulnerable to CVE-2014-0160\n", host)
 			os.Exit(1)
-		case tls.ErrNoHeartbeat:
-			fmt.Printf("SECURE - %s does not have the heartbeat extension enabled\n", host)
-		default:
-			fmt.Printf("SECURE - %s has heartbeat extension enabled but is not vulnerable\n", host)
-			fmt.Printf("This error happened while reading the response to the malformed heartbeat (almost certainly a good thing): %q\n", err)
 		}
+		fmt.Printf("SECURE - %s has heartbeat extension enabled but is not vulnerable\n", host)
+		fmt.Printf("This error happened while reading the response to the malformed heartbeat (almost certainly a good thing): %q\n", err)
 	case <-time.After(10 * time.Second):
 		fmt.Printf("SECURE - %s has the heartbeat extension enabled, but timed out after a malformed heartbeat (this likely means that it is not vulnerable)\n", host)
 	}
